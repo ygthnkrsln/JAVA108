6a5a190d63aecdce5d5a3c59c77a8adc8f82b68b
Yiğithan <forbequem@gmail.com>
2023-03-09 12:37:06 +0300

    Initial commit

---
+++ src/Lab_Studies/Lab1/Course/Course.java
@@ -0,0 +1,47 @@
+package Lab_Studies.Lab1.Course;
+
+public class Course {
+    private String courseName;
+    private String[] students = new String[100];
+    private int numberOfStudents = 0;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void addStudent(String student) {
+        students[numberOfStudents] = student;
+        numberOfStudents++;
+    }
+
+    public String[] getStudents() {
+        return students;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void dropStudent(String student) {
+        int i;
+
+        for (i = 0; i < students.length; i++) {
+            if (students[i] == null) break;
+
+            if (students[i].equals(student)) {
+                for (int j = i + 1; j < students.length; j ++) {
+                    students[j - 1] = students[j];
+                }
+
+                students[students.length - 1] = null;
+                numberOfStudents--;
+
+                break;
+            }
+        }
+    }
+}
---
+++ src/Lab_Studies/Lab1/IsStringPalindrome.java
@@ -0,0 +1,40 @@
+package Lab_Studies.Lab1;
+
+import java.util.Scanner;
+
+public class IsStringPalindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String stringInput = input.nextLine();
+
+        System.out.println("Is " + stringInput + " a palindrome without non-alphanumeric characters ? " + isPalindrome(stringInput));
+    }
+
+    public static boolean isPalindrome(String stringInput) {
+        String filteredString = filter(stringInput);
+        String reverseString = reverse(stringInput);
+
+        return reverseString.equals(filteredString);
+    }
+
+    public static String filter(String stringInput) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < stringInput.length(); i++) {
+            if (Character.isLetterOrDigit(stringInput.charAt(i))) {
+                stringBuilder.append(stringInput.charAt(i));
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static String reverse(String stringInput) {
+        StringBuilder stringBuilder = new StringBuilder(stringInput);
+        stringBuilder.reverse();
+
+        return stringBuilder.toString();
+    }
+}
